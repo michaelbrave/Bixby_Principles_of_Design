85208a2de020da8779ad02303d2bac9da42bad7e
Michael Brave <michaelbrave@outlook.com>
2019-12-17 15:19:50 -0800

    first upload

---
+++ playground.principlesOfDesign/code/content.js
@@ -0,0 +1,72 @@
+module.exports = [
+  {
+    "text": "Good design is innovative.  “The possibilities for innovation are not, by any means, exhausted. Technological development is always offering new opportunities for innovative design. But innovative design always develops in tandem with innovative technology, and can never be an end in itself.” –DIETER RAMS",
+    "image": {
+      "url": "images/10.jpg"
+    },
+    "tags": ["innovative"]
+  },
+  {
+    "text": "Good design makes a product useful.  “A product is bought to be used. It has to satisfy certain criteria, not only functional, but also psychological and aesthetic. Good design emphasizes the usefulness of a product whilst disregarding anything that could possibly detract from it.” –DIETER RAMS",
+    "image": {
+      "url": "9.jpg"
+    },
+    "tags": ["useful"]
+  },
+  {
+    "text": "Good design is aesthetic.  “The aesthetic quality of a product is integral to its usefulness because products we use every day affect our person and our well-being. But only well-executed objects can be beautiful.” –DIETER RAMS",
+    "image": {
+      "url": "images/2.jpg"
+    },
+    "tags": ["aesthetic"]
+  },
+  {
+    "text": "Good design makes a product understandable.  “It clarifies the product’s structure. Better still, it can make the product talk. At best, it is self-explanatory.” –DIETER RAMS",
+    "image": {
+      "url": "images/8.jpg"
+    },
+    "tags": ["understandable"]
+  },
+  {
+    "text": "Good design is unobtrusive.  “Products fulfilling a purpose are like tools. They are neither decorative objects nor works of art. Their design should therefore be both neutral and restrained, to leave room for the user’s self-expression.” –DIETER RAMS",
+    "image": {
+      "url": "images/7.jpg"
+    },
+    "tags": ["unobtrusive"]
+  },
+  {
+    "text": "Good design is honest.  “It does not make a product more innovative, powerful or valuable than it really is. It does not attempt to manipulate the consumer with promises that cannot be kept.” –DIETER RAMS",
+    "image": {
+      "url": "images/3.jpg"
+    },
+    "tags": ["honest"]
+  },
+  {
+    "text": "Good design is long-lasting.  “It avoids being fashionable and therefore never appears antiquated. Unlike fashionable design, it lasts many years – even in today’s throwaway society.” –DIETER RAMS",
+    "image": {
+      "url": "images/6.jpg"
+    },
+    "tags": ["long-lasting"]
+  },
+  {
+    "text": "Good design is thorough down to the last detail.  “Nothing must be arbitrary or left to chance. Care and accuracy in the design process show respect towards the user.” –DIETER RAMS",
+    "image": {
+      "url": "images/1.jpg"
+    },
+    "tags": ["thorough"]
+  },
+  {
+    "text": "Good design is environmentally friendly.  “Design makes an important contribution to the preservation of the environment. It conserves resources and minimizes physical and visual pollution throughout the lifecycle of the product.” –DIETER RAMS",
+    "image": {
+      "url": "images/5.jpg"
+    },
+    "tags": ["environmentally friendly"]
+  },
+  {
+    "text": "Good design is as little design as possible.  “Less, but better – because it concentrates on the essential aspects, and the products are not burdened with non-essentials.  Back to purity, back to simplicity.” –DIETER RAMS",
+    "image": {
+      "url": "images/4.jpg"
+    },
+    "tags": ["minimal"]
+  }
+]
